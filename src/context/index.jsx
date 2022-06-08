bf0e86ea843650d0fd1d8c60976b61db2db9f012
Felipe Hsu <felipe_hsu@hotmail.com>
2022-06-08 00:29:11 -0300

    Add register user handler
    Co-authored-by: savioalmeida <savioalmeida@users.noreply.github.com>

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,25 +1,27 @@
 import { useContext, createContext, useState } from 'react';
 import axios from 'axios';
+const API_URL = 'https://convert-dr-app.herokuapp.com'
 
 const StoreContext = createContext();
 
 export const StoreProvider = ({ children }) => {
     const [userData, setUserData] = useState(false);
+    const [userJobs, setUserJobs] = useState([]);
+    const [userCompanies, setUserCompanies] = useState([]);
     const [authToken, setAuthToken] = useState(false);
 
     const loginHandler = async ({
         password, nickname
     }) => {
         try {
-            const { data } = await axios.post('https://convert-dr-app.herokuapp.com/users/login', {
+            const { data } = await axios.post(`${API_URL}/users/login`, {
                 password: password,
                 nickname: nickname,
             })
             setAuthToken(data.auth.token)
-            setUserData(data.data)
-
-            console.log(authToken)
-            console.log(userData)
+            setUserData(data.data.user)
+            setUserJobs(data.data.jobs)
+            setUserCompanies(data.data.companies)
         } catch (err) {
             console.error(err)
             console.error('Falha durante login')
@@ -27,11 +29,36 @@ export const StoreProvider = ({ children }) => {
         }
     }
 
+    const registerHandler = async ({
+        email,
+        password,
+        nickname,
+        firstName,
+        lastName,
+    }) => {
+        try {
+            await axios.post(`${API_URL}/users/register`, {
+                email,
+                password,
+                nickname,
+                firstName,
+                lastName,
+            })
+        } catch (err) {
+            console.error(err)
+            console.error('Falha durante cadastro')
+            throw new Error('Falha durante cadastro')
+        }
+    }
+
     return (
         <StoreContext.Provider value={{ 
             loginHandler,
+            registerHandler,
             authToken,
             userData,
+            userJobs,
+            userCompanies
         }}>
             {children}
         </StoreContext.Provider>
--- src/pages/CadastroPage/index.jsx
+++ src/pages/CadastroPage/index.jsx
@@ -1,25 +1,34 @@
 import React, { useState } from "react"
 import "./stylesCadastro.css"
-
+import { useStore } from '../../context';
 //*Linkando rotas entre as views*
 import { Link } from "react-router-dom"
 
 /*Amarzenamentos dos cadastros*/
 const CadastroPage = () =>{
     <Link to ="/login"></Link>
-    const [nome, setNome] = useState("");
-    const [sobrenome, setSobrenome] = useState("");
 
+    const { registerHandler } = useStore();
+    
+    const [name, setName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [nickname, setNickname] = useState("");
+    const [password, setPassword] = useState("");
+    
     const handleSubmit = (e) => {
         e.preventDefaut();
 
-
-        console.log("submit", {nome, sobrenome});
+        registerHandler({
+            firstName: name,
+            lastName: lastName,
+            email: email,
+            password: password,
+            nickname: nickname,
+        })
+            .then(() => { alert('Succesfully register new user.')})
+            .catch(() => { alert('Failed to register new user.')})
     }
-
-
-
-
 
 //Corpo da página
     return (
@@ -36,35 +45,47 @@ const CadastroPage = () =>{
                         <div className="campo-writing">
                             <label htmlFor="nome">Nome:</label>
                             <input type="text" name="nome"
-                            className={nome !== "" ? "has-val input" : "input"}
-                            value={nome} 
-                            onChange={(e) => setNome(e.target.value)}
+                            className={name !== "" ? "has-val input" : "input"}
+                            value={name} 
+                            onChange={(e) => setName(e.target.value)}
                             required placeholder="Digite seu primeiro nome"/>
                         </div>
 
                         <div className="campo-writing">
                             <label htmlFor="sobrenome">Sobrenome:</label>
                             <input type="text" name="sobrenome" 
-                            className={sobrenome !== "" ? "has-val input" : "input"}
-                            value={sobrenome} 
-                            onChange={(e) => setSobrenome(e.target.value)}
+                            className={lastName !== "" ? "has-val input" : "input"}
+                            value={lastName} 
+                            onChange={(e) => setLastName(e.target.value)}
                             required placeholder="Digite seu segundo nome"
                             />
                         </div>
 
                         <div className="campo-writing" >
                             <label htmlFor="email">E-mail:</label>
-                            <input type="email" name="email" required placeholder="Digite seu e-mail pessoal" />
+                            <input 
+                            type="email"
+                            name="email" required 
+                            value={email} 
+                            placeholder="Digite seu e-mail pessoal"
+                            onChange={(e) => setEmail(e.target.value)} />
                         </div>
 
                         <div className="campo-writing">
                             <label htmlFor="nickname">Nickname: </label>
-                            <input type="txt" name="telefone"  required placeholder="Digite uma palavra passe"/>
+                            <input type="txt" name="telefone"  
+                            required
+                            value={nickname} 
+                            placeholder="Digite o nome de usuário" 
+                            onChange={(e) => setNickname(e.target.value)} />
                         </div>
 
                         <div className="campo-writing">
                             <label htmlFor="senha">Senha:</label>
-                            <input type="password" name="senha" required placeholder="Digite sua senha" />
+                            <input type="password" name="senha" 
+                            value={password} 
+                            required placeholder="Digite sua senha" 
+                            onChange={(e) => setPassword(e.target.value)}/>
                         </div>
 
                         <div className="actionsCadastro">
